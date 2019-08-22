a15a828b4bf95cf28ff4943be863daadf1d0646f
novinary <novina.dev@gmail.com>
2019-08-22 14:03:10 +0100

    Worked on the route changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navingations';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import './App.css';
 
@@ -18,15 +19,45 @@ const particlesOptions = {
   }
 };
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Signin />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'signin',
+      isSignedIn: false
+    };
+  }
+
+  onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false });
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedIn, route } = this.state;
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+          </div>
+        ) : route === 'signin' ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Register extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        email: '',
+        password: '',
+        name: ''
+      }
+    }
+
+    render() {
+        return (
+    <div>hi</div>
+        );
+      }
+    }
+
+export default Register;
