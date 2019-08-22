496261be22dc36182cb8e76ac0e19c3ca64ddade
novinary <novina.dev@gmail.com>
2019-08-22 13:24:43 +0100

    Added interactive background using react-particles-js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import Navigation from './components/Navingations'
-import Logo from './components/Logo/Logo'
+import Particles from 'react-particles-js';
+import Navigation from './components/Navingations';
+import Logo from './components/Logo/Logo';
 import './App.css';
+
+// react particles configuration
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-    <Navigation />
-    <Logo />
+    <div className='App'>
+      <Particles className='particles' params={particlesOptions} />
+      <Navigation />
+      <Logo />
     </div>
   );
 }
