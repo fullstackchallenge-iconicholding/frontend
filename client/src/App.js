4068e68777407bf15b29bad39d38012a7b0d8f1e
novinary <novina.dev@gmail.com>
2019-08-22 12:58:35 +0100

    Built skeleton for navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './components/Navingations'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Navigation />
     </div>
   );
 }
